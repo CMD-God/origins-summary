01ae5f159017c7d7c314f2c116a3dac2809fe2be
CMD-God <48065803+CMD-God@users.noreply.github.com>
2023-06-21 12:18:00 +0300

    Upload Time

--- js/create_summaries.js
+++ js/create_summaries.js
@@ -1,4 +1,8 @@
 
+var origins_language_data;
+var my_powers;
+var my_origins;
+
 const PLURAL_LAYER_NAMES = {
     "origins:origin" : "Origins",
     "mage_specializations": "Mage Specializations"
@@ -19,7 +23,7 @@ function createOriginLists(layers) {
         for (var j=0; j < layer.origins.length; j++) {
             var origin_obj = layer.origins[j];
             if (typeof(origin_obj) == "string") {
-                list.push(origin_list_obj);
+                list.push(origin_obj);
             } else {
                 list = list.concat(origin_obj.origins);
             }
@@ -33,9 +37,53 @@ function createOriginLists(layers) {
     return origin_lists;
 }
 
-$.getJSON("./UCR_Origins_Backup.json", {}, (data) => {
-    console.log(data);
-    var layers = data["origin_layers/"];
-    var origin_lists = createOriginLists(layers);
-    console.log(origin_lists);
+function followSlashObjects(parent = {}, path = "") {
+    var split_path = path.split("/");
+    for (var i=0; i < split_path.length -1; i++) {
+        parent = parent[split_path[i] + "/"];
+    }
+    return parent[split_path[split_path.length-1]];
+}
+
+function fetchNameAndDescForPower(power_id = "") {
+    var name;
+    var desc;
+    var bits = power_id.split(":");
+    var namespace = bits[0];
+    var id = bits[1];
+    if (!(namespace === "ucr_stitch")) {
+        // We assume the power is from Origins.
+        name = origins_language_data[`power.origins.${id}.name`];
+        desc = origins_language_data[`power.origins.${id}.description`];
+    } else {
+        var power = followSlashObjects(my_powers, id);
+        name = power.name;
+        desc = power.description;
+    }
+
+    return {name: name, desc: desc};
+}
+
+function createHtmlForOrigin(origin_id = "") {
+    var bits = origin_id.split(":");
+    var namespace = bits[0];
+    var id = bits[1];
+    
+    var origin_obj = followSlashObjects(my_origins, id);
+    var origin_name = origin_obj.name || origins_language_data[`origin.origins.${id}.name`];
+    var origin_desc = origin_obj.description || origins_language_data[`origin.origins.${id}.description`];
+    
+    var html = `<div><h1>${origin_name}</h1><p>${origin_desc}</p></div>`;
+    return html;
+}
+
+$.getJSON("./origins_en_us.json", {}, (lan_data) => {
+    origins_language_data = lan_data;
+    $.getJSON("./UCR_Origins_Backup.json", {}, (data) => {
+        my_powers = data["powers/"];
+        my_origins = data["origins/"];
+        var layers = data["origin_layers/"];
+        var origin_lists = createOriginLists(layers);
+        console.log(origin_lists);
+    });
 });
